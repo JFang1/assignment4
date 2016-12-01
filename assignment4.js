572250ed3563db3571abcfa487b3baf382ded23a
JFang1 <jmf133@pitt.edu>
2016-12-01 13:18:49 -0500

    Successfully retrieved the JSON data

--- assignment4.js
+++ assignment4.js
@@ -11,7 +11,75 @@
 // $.html(), $.text(), etc.
 // keyup events could be helpful to get value of field as the user types
 
+// referenced: http://api.jquery.com/jquery.getjson/
 (function() {
-  // Magic!
   console.log('Keepin\'n it clean with an external script!');
+
+  $('.predicted-results').hide();
+
+  var returnedData;
+  var statusCode;
+  var keysArray = [];
+  var valsArray = [];
+
+  $.getJSON( "http://www.mattbowytz.com/simple_api.json?data=all", function(data) {
+    $.each(data, function(key, val) {
+      if (key==="status") {
+        statusCode = val;
+        console.log(statusCode);
+        if (val != 200) {
+          alert("Request failed!");
+        }
+      }
+      if (key==="data") {
+        returnedData=val;
+        console.log("This is the data: ");
+        console.log(returnedData);
+      }
+    });
+
+    if (statusCode == 200) {
+      JSON.parse(returnedData);
+
+      // valsArray = returnedData.toString().split(/,| /);
+      // var i;
+      // for (i=0; i<valsArray.length; i++) {
+      //   console.log(valsArray[i].toString());
+      // }
+
+      //   "class": "results",
+    //    html: items.join( "" )
+    //  $('.predicted-results').html(items.toString());
+    } // end if (statusCode == 200)
+
+  });
+
+
 })();
+
+
+$('.flexsearch-input').keyup(function(){
+  var results = [];
+  var input = document.getElementById('enteredInput').value;
+  if (input == "" || input == null) {
+    console.log('Nothing in the flexsearch-input!');
+  }
+  else {
+    console.log(input);
+  }
+  // if (input == "" || input == null) {
+  //   $('.predicted-results').hide();
+  // }
+  // else {
+  //   var mBowytzAPI = "http://www.mattbowytz.com/simple_api.json?data=all";
+  //   $.getJSON( mBowytzAPI).done(function( data ) {
+  //     $.each( data.items, function( i, item ) {
+  //       //$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+  //       if ( i === 3 ) {
+  //         return false;
+  //       }
+  //     });
+  //   });
+
+  //$('.predicted-results').show();
+})
