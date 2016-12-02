27eb9052da96556ff9d1de5fd0281b9859f92bf8
JFang1 <jmf133@pitt.edu>
2016-12-01 19:49:07 -0500

    Prevents the predicted-results div from showing when there are no predicted results

--- assignment4.js
+++ assignment4.js
@@ -8,52 +8,51 @@ var dataArray = [];
 
 // The anonymous function below will fire on page load
 (function() {
-
-  $.ajax({
+  $.ajax( { // AirBnb's JavaScript style guide suggests adding an extra space before the opening brace
     dataType: 'json',
     method: 'GET',
     url: 'http://www.mattbowytz.com/simple_api.json?data=all',
-    success: function(data){
-      $.each(data, function(key, value){
+    success: function(data) {
+      $.each(data, function(key, value) {
         if (key === 'data') {
           returnedData = value;
           $.each(returnedData, function(subKey, subValue) {
             dataArray.push(subKey.toLowerCase()); // might as well push the names of the data categories too!
-            $.each(subValue, function(index, element){
+            $.each(subValue, function(index, element) {
               dataArray.push(element.toLowerCase());
             });
           });
         } // end if
       }) // end each
     }, // end success
-    error: function(data){
+    error: function(data) {
       console.log(data);
     } // end error
   }); // end ajax call
-
   $('.predicted-results').hide();
-
 })(); // end main function
 
 
-$('.flexsearch-input').keyup(function(){
-
+$('.flexsearch-input').keyup(function() {
   var resultsStr = '<ul>';
   var input = document.getElementById('enteredInput').value.toLowerCase();
+  var show = 0;
 
-  if (input == '' || input == null) {
+  if (input === '' || input === null) {
     $('.predicted-results').hide();
   }
   else {
-    $.each(dataArray, function(dataIndex, dataValue){
+    $.each(dataArray, function(dataIndex, dataValue) {
       if (dataValue.startsWith(input)) {
         resultsStr = resultsStr + '<li><a target=\'_blank\' href=\"http://www.google.com/search?q=' + dataValue.replace(/ /g, '+') + '\">' + dataValue + '</li>';
+        show = 1;
       }
     });
-    resultsStr = resultsStr + "</ul>";
-    $('.predicted-results').html(resultsStr).show();
+    resultsStr = resultsStr + '</ul>';
+    if (show === 1) {
+      $('.predicted-results').html(resultsStr).show();
+    }
   } // end else
-
 }); // end keyup handling
 
 
